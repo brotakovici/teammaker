180d9a37be1484a5ae4bd5c7272f8395a3a298fb
brotakovici <brotakovici@gmail.com>
2015-06-12 17:19:07 +0300

    Added a priority q

--- Maker.rb
+++ Maker.rb
@@ -1,14 +1,21 @@
-require_relative  "TeamSizeError"
+require_relative  "TeamSizeError.rb"
+require_relative "Player.rb"
 
 class Maker
+  
   def initialize(names, teamSize, pairNo)
     @nameList = names
     @size = teamSize
     @no = pairNo
   end 
 
+  def orderByPriority(array)
+    array.sort {|x, y| x.getPriority > y.getPriority}  
+  end
+  
   def makePair
     shuffledNames = @nameList.shuffle
+    shuffledNames = orderByPriority(shuffledNames)
     
     if shuffledNames.length < @size * 2
       raise TeamSizeError.new(@size, shuffledNames.size)
@@ -17,7 +24,8 @@ class Maker
     teamOne = shuffledNames[0..@size - 1]
     teamTwo = shuffledNames[@size..@size * 2 - 1]
     remainder = shuffledNames - teamOne - teamTwo
-  
+    (shuffledNames - remainder).map {|a| a.decreasePriority}
+
     pair = [teamOne, teamTwo, remainder]
    return pair 
   end
