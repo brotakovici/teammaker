c2fa7d0a7fafb2b25a88e0d9445c5a232b023083
brotakovici <brotakovici@gmail.com>
2015-06-12 14:42:37 +0300

    Added an input and output handler for high cohesion

---
+++ IOHandler.rb
@@ -0,0 +1,39 @@
+class IOHandler
+  
+  def initialize(input, output)
+    @inputFile = input
+    @outputFile = output
+  end
+
+  def readNames
+    names = Array.new
+    file = File.open(inputFile, "r")
+    
+    while !file.eof?
+      names.push(file.readline)
+    end
+
+    file.close
+    return names
+  end
+
+  def writeNames(team1, team2, remainder)
+    outfile = File.open(outputFile, "w")
+    
+    outfile.puts "Team 1"
+    team1.each do |nick|
+      outfile.puts nick
+    end 
+
+    outfile.puts "\nTeam2"
+    team2.each do |nick|
+      outfile.puts nick
+    end
+    
+    outfile.puts "\nNot in this game"
+    remainder.each do |nick|
+      outfile.puts nick
+    end
+    outfile.close
+  end
+end 
